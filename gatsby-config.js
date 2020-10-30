45639b5dd609a18d2a8d45d22b0af352ea2b89c0
viveknigam3003 <viveknigam.nigam3@gmail.com>
2020-10-30 16:17:16 +0530

    Added WebFonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,27 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-webfonts`,
+      options: {
+        fonts: {
+          google: [
+            {
+              family: "Roboto",
+              variants: ["300", "400", "500", "700"],
+            },
+            {
+              family: "Karla",
+              variants: ["400", "700"],
+            },
+          ],
+        },
+        formats: ['woff2', 'woff'],
+        //useMinify: true,
+        //usePreload: true,
+        //usePreconnect: false,
+      },
+    },
+  ],
 }
