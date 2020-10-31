b3b7eca3c03097eff3f369680afad62f484914e1
viveknigam3003 <viveknigam.nigam3@gmail.com>
2020-10-31 18:52:18 +0530

    Towards #4: Fix Develop and Prod Styling Issue

--- src/templates/Community.js
+++ src/templates/Community.js
@@ -38,43 +38,45 @@ const useStyles = makeStyles(theme => ({
     textTransform: "none",
   },
   img: {
-      position: "relative",
-      bottom: "0rem",
-      right: "0rem"
-  }
+    position: "relative",
+    bottom: "0rem",
+    right: "0rem",
+  },
 }))
 
 function Community() {
   const classes = useStyles()
   return (
-    <Paper className={classes.root} elevation={1}>
-      <Typography variant="h2" className={classes.h2}>
-        <span className={classes.logo}>Resuminator</span> is a community driven
-        software.
-      </Typography>
-      <Typography className={classes.subtext} component="h3" variant="h5">
-        An amazing developer and designer community works towards making
-        Resuminator better each day. Help us make it better for everyone!
-      </Typography>
-      <Button
-        className={classes.button}
-        variant="outlined"
-        color="primary"
-        size="large"
-        startIcon={<FiAlertCircle />}
-        endIcon={<FiArrowRight />}
-        href="https://github.com/viveknigam3003/resuminator/issues?q=is%3Aissue+is%3Aopen+label%3A%22good+first+issue%22"
-      >
-        <Typography
-          className={classes.buttonText}
-          variant="subtitle1"
-          color="inherit"
-        >
-          {" "}
-          Find issues to solve
+    <React.Fragment>
+      <Paper className={classes.root} elevation={1}>
+        <Typography variant="h2" className={classes.h2}>
+          <span className={classes.logo}>Resuminator</span> is a community
+          driven software.
         </Typography>
-      </Button>
-    </Paper>
+        <Typography className={classes.subtext} component="h3" variant="h5">
+          An amazing developer and designer community works towards making
+          Resuminator better each day. Help us make it better for everyone!
+        </Typography>
+        <Button
+          className={classes.button}
+          variant="outlined"
+          color="primary"
+          size="large"
+          startIcon={<FiAlertCircle />}
+          endIcon={<FiArrowRight />}
+          href="https://github.com/viveknigam3003/resuminator/issues?q=is%3Aissue+is%3Aopen+label%3A%22good+first+issue%22"
+        >
+          <Typography
+            className={classes.buttonText}
+            variant="subtitle1"
+            color="inherit"
+          >
+            {" "}
+            Find issues to solve
+          </Typography>
+        </Button>
+      </Paper>
+    </React.Fragment>
   )
 }
 
