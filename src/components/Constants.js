17440357a5c927fd4fe0490ff154cd110ef38278
viveknigam3003 <viveknigam.nigam3@gmail.com>
2020-11-02 19:32:10 +0530

    Introduced Constants

---
+++ src/components/Constants.js
@@ -0,0 +1,4 @@
+export const RESUMINATOR_REPO_LINK = "https://github.com/viveknigam3003/resuminator";
+export const RESUMINATOR_LANDING_REPO_LINK = "https://github.com/viveknigam3003/resuminator-landing"
+export const RESUMINATOR_LANDING_HOSTED_LINK = "https://resuminator.gtsb.io/"
+export const OWNER_PROFILE = "https://resuminator.gtsb.io/"
