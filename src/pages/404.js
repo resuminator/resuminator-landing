956097c1acd477c8345100c3a4947af199edbbe0
chinmaym07 <b418020@iiit-bh.ac.in>
2020-11-11 13:21:17 +0530

    Added Some Animation to the Error Page

--- src/pages/404.js
+++ src/pages/404.js
@@ -12,11 +12,13 @@ import { Box, Button, makeStyles, Typography } from "@material-ui/core"
 import NOT_FOUND_IMAGE from "../images/undraw_not_found_60pq.svg"
 import React from "react"
 import { FiArrowLeft } from "react-icons/fi"
+import { motion } from 'framer-motion';
 
 const useStyles = makeStyles(theme => ({
   root: {
     minHeight: "100vh",
     width: "auto",
+    
     backgroundColor: theme.palette.primary.dark,
     "@media (min-width:1280px)": {
       padding: "16rem",
@@ -34,7 +36,8 @@ const useStyles = makeStyles(theme => ({
   title: {
     fontFamily: "Karla",
     fontWeight: 700,
-    color: theme.palette.secondary.light,
+    fontSize:'5rem',
+    color: theme.palette.secondary.light
   },
   subtitle: {
     color: theme.palette.primary.light,
@@ -43,10 +46,15 @@ const useStyles = makeStyles(theme => ({
   },
   img: {
     paddingTop: "5rem",
+    height:"18rem"
   },
   backBtn: {
     color: theme.palette.secondary.light,
-    marginTop: "2rem"
+    marginTop: "2rem",
+    backgroundColor: theme.palette.primary.dark,
+    border:"none",
+    outline:"none",
+    
   },
   btnText: {
     fontSize: "1.2rem",
@@ -67,30 +75,60 @@ function Page404() {
       p={5}
       pt={10}
     >
-      <Typography variant="h3" className={classes.logo}>
+      <motion.Typography
+        variant="h3"
+        className={classes.logo}
+        initial={{opacity:0,y:"-100px"}}
+        animate={{opacity:1,y:0}}
+        transition={{type:'spring',duration:3}}
+        >
         Resuminator
-      </Typography>
-      <Typography className={classes.title} color="secondary" variant="h1">
+      </motion.Typography>
+      <motion.Typography 
+        className={classes.title} 
+        color="secondary" 
+        variant="h1"
+        initial={{x:'100vw'}}
+        animate={{x:0}}
+        transition={{type:'spring',duration:2}}
+      >
         404
-      </Typography>
-      <Typography className={classes.subtitle} variant="h2">
+      </motion.Typography>
+      <motion.Typography className={classes.subtitle} variant="h2"
+         initial={{y:'300px'}}
+        animate={{y:0}}
+        transition={{type:'spring',duration:2}}>
         Oops, looks like you landed on a wrong address!
-      </Typography>
-      <Button
-        startIcon={<FiArrowLeft />}
+      </motion.Typography>
+      <motion.Button 
         variant="text"
         className={classes.backBtn}
         href="/"
+        initial={{y:'300px'}}
+        animate={{y:0}}
+        transition={{type:'spring',duration:2}}
+        whileHover={{
+            scale:1.1,
+            textShadow:`0px 0px 16px #ff4081`,
+            
+            }}
       >
-        <Typography variant="button" className={classes.btnText}>Back to awesomeness</Typography>
-      </Button>
-      <img
+        <FiArrowLeft style={{fontSize:"1.2rem",marginRight:"4px"}}/>
+        <Typography variant="button" className={classes.btnText} >
+          
+            Back to awesomeness
+        </Typography>
+      </motion.Button>
+      <motion.img
         className={classes.img}
         src={NOT_FOUND_IMAGE}
         alt="Err_404_Not_Found"
         height="190rem"
         width="auto"
         style={{paddingTop: "2rem"}}
+        initial={{y:'300px'}}
+        animate={{y:0}}
+        transition={{type:'spring',duration:2}}
       />
     </Box>
   )
