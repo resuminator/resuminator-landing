9c726566a713d41e5bda026eac942a245bcc09a0
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-01-13 23:33:13 +0530

    Added Micro-interractions on button

--- src/components/Home/ActionButtons.js
+++ src/components/Home/ActionButtons.js
@@ -8,42 +8,47 @@
  * - Vivek Nigam, <viveknigam.nigam3@gmail.com>, 2020
  */
 
-import { Button, makeStyles, Typography } from "@material-ui/core"
+import { Button, makeStyles } from "@material-ui/core"
+import { motion } from "framer-motion"
 import React from "react"
 import { FiArrowRight } from "react-icons/fi"
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   button: {
     padding: "0.8rem",
     margin: "1rem 0 2rem 0",
-  },
-  buttonText: {
+    color: theme.palette.text.light,
     fontFamily: "Manrope",
     fontWeight: 700,
+    letterSpacing: 0.5,
     textTransform: "none",
+    "&:hover, &:active": {
+      color: theme.palette.secondary.light,
+    },
   },
-})
+}))
 
 const ActionButton = ({ onClick }) => {
   const classes = useStyles()
+
   return (
-    <Button
-      size="large"
-      color="primary"
-      variant="contained"
-      className={classes.button}
-      endIcon={<FiArrowRight color="inherit" />}
-      disableElevation
-      onClick={onClick}
+    <motion.div
+      whileHover={{ scale: 1.1 }}
+      whileTap={{scale: 0.9}}
+      transition={{ duration: 0.15 }}
     >
-      <Typography
-        className={classes.buttonText}
-        variant="body1"
-        color="textSecondary"
+      <Button
+        size="large"
+        color="primary"
+        variant="contained"
+        className={classes.button}
+        endIcon={<FiArrowRight color="inherit" />}
+        disableElevation
+        onClick={onClick}
       >
         Join the Beta waitlist
-      </Typography>
-    </Button>
+      </Button>
+    </motion.div>
   )
 }
 
